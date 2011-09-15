77fa9721aef25ce66e8e409a3b878036c18056f6
Erik Nilsson <eriknilsson89@gmail.com>
2011-09-15 20:05:13 +0200

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return a title
+  def title
+    base_title = "The Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
